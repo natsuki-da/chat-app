16490e95fb7070d9e1a813968406bddccae9ec61
Natsuki Iida <natsuki.iida0511@gmail.com>
2025-11-12 13:09:56 +0100

    minor change

--- src/main/java/se/sprinto/hakan/chatapp/ChatServerMain.java
+++ src/main/java/se/sprinto/hakan/chatapp/ChatServerMain.java
@@ -3,5 +3,7 @@ package se.sprinto.hakan.chatapp;
 public class ChatServerMain {
     public static void main(String[] args) {
         new ChatServer(5555).start();
+//        TestDBConnection testDB = new TestDBConnection();
+//        testDB.connectToDB();
     }
 }
